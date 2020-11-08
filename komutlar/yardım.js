d3f1ccd191156d35f5e0841bdbceadee44bd548a
Glitch (chip-living-peridot) <none>
2020-11-08 07:23:54 +0000

    😻⚾️ Checkpoint ./ayarlar.json:14097343/218 ./package.json:14097343/2256 ./komutlar/yardım.js:14097343/5 ./spamkorumasi.js:14097343/181

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -1,4 +1,4 @@
-﻿const Discord = require('discord.js');
+const Discord = require('discord.js');
 const ayarlar = require('../ayarlar.json');
 exports.run = async (client, message, args) => {
    const filter = (reaction, user) => {
