936e7e48282eac6b367de7249fc6d3477cd6ab94
Glitch (salty-subsequent-pentaceratops) <none>
2020-11-08 15:45:29 +0000

    🎯🎫 Checkpoint ./komutlar/yardım.js:24336266/51

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -7,7 +7,7 @@ exports.run = async (client, message, args) => {
 
   const yardım = new Discord.MessageEmbed()
     .setTitle(`${message.author.username} - Tarafından İstendi`)
-      .setColor("PURPLE")
+      .setColor("GREEN")
   .setAuthor(client.user.username, client.user.avatarURL())
   .setDescription(`**Davet Komutları: ➕ \n Ana Menü: 💡**`)
   .setImage("https://images-ext-1.discordapp.net/external/Bb032GyJs8yCJiUy7tWQ-YnNRPreLuPDo-xp66eOIeU/https/images-ext-2.discordapp.net/external/H1PQhcDr-EaEvwENT8cUxj8S2yonFZl351YbXXH5sGs/https/media.discordapp.net/attachments/697145772801785876/716671769355747348/1.gif")
@@ -22,7 +22,7 @@ collector.on('collect', (reaction, user) => {
   
      if(reaction.emoji.name == "➕") {
     const kobscode = new Discord.MessageEmbed()
-      .setColor("PURPLE")
+      .setColor("BLUE")
  .addField("**Davet Komutları**", `\n**!**__rütbe-ekle @rol davet__ = **__Rütbe Ekler__** \n**!**__rütbeler__ = **__Rütbeleri Gösterir 1 Den 10'a Kadar__** \n**!**__rütbe-sıfırla__ = **__Rütbeyi Sıfırlar.__** \n**!**__davetleri-sıfırla__ = **__Davetleri Sıfırlar.__** \n**!**__top__ = **__Lider Tablosunu Gösterir.__** \n**!**__davetlerim__ = **__Davetlerinizi Gösterir.__** \n**!**__bonus-ekle__ = **__Bonus Ekler.__** \n**!**__davet-kanal #kanal__ = **__Davet Kanalını Ayarlar.__** \n**!**__davet-kanal-sıfırla__ = **__Davet Kanalını Sıfırlar.__**`)
 .setImage("https://cdn.discordapp.com/attachments/772883062085386282/775006230082748436/3.png")
   .setThumbnail(client.user.avatarURL())
